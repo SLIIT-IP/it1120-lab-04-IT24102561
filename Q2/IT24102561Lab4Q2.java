cdb1ce8cbae34f8665ba735a659f4e5a997c1713
IT24102561 <it24102561@my.sliit.lk>
2024-08-18 19:02:41 +0530

    Lab4Q2

---
+++ Q2/IT24102561Lab4Q2.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class IT24102561Lab4Q2{
+   public static void main(String[] args){
+      Scanner input = new Scanner (System.in);
+
+      System.out.print("Enter the exam marks(0-100): ");
+      int exammarks = input.nextInt();
+if (exammarks < 0 || exammarks > 100){
+     System.out.println("Invalid exam marks.Must be between 0 and 100.");
+  }
+     System.out.print("Enter the lab submission marks(0-100): ");
+      int labmarks = input.nextInt();
+if (labmarks < 0 || labmarks > 100){
+     System.out.println("Invalid lab submission marks.Must be between 0 and 100.");
+
+}
+    System.out.print("Enter thepercentage for exam marks: ");
+      double exampercentage = input.nextDouble();
+
+System.out.print("Enter thepercentage for labsumission marks: ");
+      double labpercentage = input.nextDouble();
+
+
+if (exampercentage + labpercentage != 100){
+     System.out.print("The percentage must add up to 100.");
+  }
+   double finalmark = (exammarks*(exampercentage / 100) + (labmarks * (labpercentage / 100)));
+   System.out.printf("The final mark is: %2f%n", finalmark);
+
+   
+   }
+}
